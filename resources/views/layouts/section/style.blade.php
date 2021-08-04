a4f395b0908e278ac3db0727ac2d0b3849ef5721
almas-alright <kutsnalmas@gmail.com>
2021-08-04 19:44:12 +0600

    template asset linked

--- resources/views/layouts/section/style.blade.php
+++ resources/views/layouts/section/style.blade.php
@@ -1,5 +1,4 @@
-<link href="{{asset('/')}}public/backend/css/bootstrap.min.css" rel="stylesheet">
-<link href="{{asset('/')}}public/backend/font-awesome/css/font-awesome.css" rel="stylesheet">
-
-<link href="{{asset('/')}}public/backend/css/animate.css" rel="stylesheet">
-<link href="{{asset('/')}}public/backend/css/style.css" rel="stylesheet">
+<link href="{{asset('backend/css/bootstrap.min.css')}}" rel="stylesheet">
+<link href="{{asset('backend/font-awesome/css/font-awesome.css')}}" rel="stylesheet">
+<link href="{{asset('backend/css/animate.css')}}" rel="stylesheet">
+<link href="{{asset('backend/css/style.css')}}" rel="stylesheet">
