e6bc55fe83f005b1b2dcb1b0b04531a292237ad6
Smo Shjaib <smoshajib100@gmail.com>
2021-08-08 14:43:09 +0600

    Update AdminDashboard

--- routes/admin.php
+++ routes/admin.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\Admin\Auth\AuthController;
 use App\Http\Controllers\Admin\AdminDashboardController;
 
 Route::get('/', function () {
-    return view('admin.auth.login');
+    return view('cms.auth.login');
 });
 
 Route::get('dashboard', [AdminDashboardController::class, 'index'])->name('admin.dashboard');
